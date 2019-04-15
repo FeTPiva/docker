687b868e9962210d0ad2bb57f7faed480f63dfee
Fernanda Piva <fernanda.piva@br.promonlogicalis.com>
2019-04-15 18:50:14 -0300

    ideia maluca de login

---
+++ FrontEnd/js/login.js
@@ -0,0 +1,46 @@
+
+//https://www.formget.com/javascript-login-form/
+
+var attempt = 3; // Variable to count number of attempts.
+// Below function Executes on click of login button.
+function validate() {
+    //isso ja sai como string https://www.w3schools.com/jsref/prop_text_value.asp
+    var username = document.getElementById("email").value;
+    var password = document.getElementById("senha").value;
+
+    //var url = document.location.protocol + "//" + document.location.hostname + ":9002";
+    //var api = url + "/api/emails/custom"
+
+    $.ajax({
+
+
+        url: "IP://docker/APILogin/HotelSlim/public/v1/funcionario:PORTA", // my php file
+        type: 'GET', // type of the HTTP request
+        success: function (funcionariosArray) {
+            var obj = jQuery.parseJSON(result);
+            console.log(obj);
+
+            var data = {};
+            if (username.equals(data["login"]) && password.equals(data["senha"])) {
+
+                alert("Login successfully");
+                window.location = "exibe.html"; // Redirecting to other page.
+                return false;
+
+            }
+
+            else {
+                attempt--;// Decrementing by one.
+                alert("You have left " + attempt + " attempt;");
+                // Disabling fields after 3 attempts.
+                if (attempt == 0) {
+                    document.getElementById("email").disabled = true;
+                    document.getElementById("senha").disabled = true;
+                    document.getElementById("login").disabled = true;
+                    return false;
+                }
+            }
+        }
+    });
+
+}
